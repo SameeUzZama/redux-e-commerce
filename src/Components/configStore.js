7637e2c9e2e9b937afe4f7ed564b8bd119dea155
Samee Uz Zama <94825943+SameeUzZama@users.noreply.github.com>
2022-11-03 12:28:45 +0530

    Updates

--- src/Components/configStore.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { legacy_createStore as createStore } from "redux";
-import { reducer } from "./reducer";
-
-export const configStore = () => {
-  const myStore = createStore(combineReducers({ reducer }));
-  return myStore;
-};
